f2d79bbc009f3774e45012d0ab816cbe299f216d
Matias ibanez <ibanezgarciamatias@gmail.com>
2024-03-17 23:23:14 -0300

    fourth commit

--- main.js
+++ main.js
@@ -13,10 +13,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // Verificar las credenciales
             if (data.username === 'mibaez@arcor.com' && data.password === '12345') {
-                // Credenciales correctas, redirigir al usuario a search.html
-                window.location.href = './pages/search.html';
+                // Mostrar confirmación antes de redirigir
+                const confirmation = confirm('¿Estás seguro de que deseas continuar?');
+                
+                if (confirmation) {
+                    // Credenciales correctas, redirigir al usuario a search.html
+                    window.location.href = './pages/search.html';
+                    
+                    // Agregar al historial de navegación
+                    history.pushState(null, '', './pages/search.html');
+                } else {
+                    // El usuario canceló la acción
+                    alert('Acción cancelada por el usuario.');
+                }
             } else {
-                // Credenciales incorrectas, mostrar un mensaje de error
+                // Credenciales incorrectas, mensaje de error
                 alert('Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.');
             }
         });
